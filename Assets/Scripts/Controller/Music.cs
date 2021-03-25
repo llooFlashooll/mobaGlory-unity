fff33dd37242c87505e688299d8240b07c3f8317
llooFlashooll <1330315641@qq.com>
2021-03-25 21:52:36 +0800

    Update Final.

---
+++ Assets/Scripts/Controller/Music.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource audio;
+    private int count = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+           Application.Quit();
+
+        }
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            count = count + 1;
+            if(count % 2 == 0)
+            {
+                audio.Play();
+            }
+            else if(count % 2 == 1)
+            {
+                audio.Pause();
+            }
+            
+        }
+    }
+}
